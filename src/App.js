0df7760d1be224427e0c124e4089c83c767ac128
Luis Cubillo <luis.cubillo.soto@gmail.com>
2021-10-03 19:22:54 -0600

    Application placeholder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+// Components
+import Folders from "./Components/Folders/Folders";
+import Notes from "./Components/Notes/Notes";
+import Editor from "./Components/Editor/Editor";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Folders />
+        <Notes />
+        <Editor />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Notes/Notes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Notes = () => {
+    return (
+        <p>Notes component</p>
+    )
+}
+
+export default Notes;
