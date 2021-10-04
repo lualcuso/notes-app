98a09081d83275fb24eb3ea0e6773e5610ae6237
Luis Cubillo <luis.cubillo.soto@gmail.com>
2021-10-04 12:05:51 -0600

    Tweaks styles

--- src/Components/Editor/Editor.js
+++ src/Components/Editor/Editor.js
@@ -5,6 +5,9 @@ import './Editor.scss'
 const Editor = () => {
     return (
         <div className="editor">
+            <div className="actions">
+                <button><i className="fas fa-plus"/></button>
+            </div>
 
         </div>
     )
